8eaa47342151ba6787f2dd3812fc32af86969f1a
vakuj <vakujrvi@gmail.com>
2022-10-19 23:45:05 +0200

    simplified the LOG macro

--- logger.cpp
+++ logger.cpp
@@ -19,7 +19,7 @@ void log_out(const char *_lvl, const char *_file, int _line, const char *_msg)
 }
 
 #ifdef DEBUG
-#define LOG(_lvl, _file, _line, _msg) log_out(_lvl, _file, _line, _msg)
+#define LOG(_lvl, _msg) log_out(_lvl, __FILE__, __LINE__, _msg)
 #else
-#define LOG(_lvl, _file, _line, _msg)
+#define LOG(_lvl, _msg)
 #endif
