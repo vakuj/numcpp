89a4eb9f5978ffb51f6872955ab67d01e7a8473b
vakuj <vakujrvi@gmail.com>
2022-10-26 22:14:06 +0200

    fixed some typos

--- NumCpp_defs.h
+++ NumCpp_defs.h
@@ -6,8 +6,8 @@
 #define NF_DIAG(n) NumCpp<float>::diag(n)
 
 #define NF_ZERO(m, n) NumCpp<float>::zero(m, n)
-#define NF_ONES(m, n) NumCpp<float> NF::ones(m, n)
-#define NF_FILL(m, n) NumCpp<float> NF::fill(m, n)
+#define NF_ONES(m, n) NumCpp<float>::ones(m, n)
+#define NF_FILL(m, n) NumCpp<float>::fill(m, n)
 
 #define NF_LINSPACE(v0, v1, n) NumCpp<float>::linspace(v0, v1, n)
 #define NF_ARANGE(v0, v1, d) NumCpp<float>::arange(v0, v1, d)
